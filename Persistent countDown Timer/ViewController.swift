baa466a60b130ee6492813e9d9599a9dba0ab3bb
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2017-09-16 16:17:02 +0300

    1st commit. Initializing repository

---
+++ Persistent countDown Timer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Persistent countDown Timer
+//
+//  Created by Manolescu Mihai Alexandru on 16/09/2017.
+//  Copyright © 2017 Manolescu Mihai Alexandru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
---
+++ Persistent countDown TimerTests/Persistent_countDown_TimerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Persistent_countDown_TimerTests.swift
+//  Persistent countDown TimerTests
+//
+//  Created by Manolescu Mihai Alexandru on 16/09/2017.
+//  Copyright © 2017 Manolescu Mihai Alexandru. All rights reserved.
+//
+
+import XCTest
+@testable import Persistent_countDown_Timer
+
+class Persistent_countDown_TimerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
